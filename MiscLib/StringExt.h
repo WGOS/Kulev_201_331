a93da2b9f7baa9e7f497f65c70ab220136c6090f
Egor <thewgosplayer@gmail.com>
2020-11-07 14:24:21 +0300

    Add string extension methods to mislib

--- MiscLib/StringExt.h
+++ MiscLib/StringExt.h
@@ -1,21 +1,26 @@
 #pragma once
-
 #include <string>
 
-namespace MiscLib
+namespace Misc
 {
-	class StringExt
+	namespace Str
 	{
-	public:
 		/// <summary>
-		/// Split strings to array of substrings by delimiter
+		/// Split strings to substrings by delimiter
 		/// </summary>
 		/// <param name="str">String to split</param>
 		/// <param name="delimiter">Delimiter to split string by</param>
 		/// <param name="subStrings">Pointer address of sub-strings array</param>
 		/// <returns>Size of sub-strings array</returns>
-		size_t split(const std::string& str, const char delimiter, std::string*& subStrings);
-	};
+		size_t Split(const std::string& str, const char delimiter, std::string*& subStrings);
+
+		/// <summary>
+		/// Join substrings into one string
+		/// </summary>
+		/// <param name="subStrings">Array of substrings to join</param>
+		/// <param name="size">Size of array of substrings</param>
+		/// <param name="delimiter">Delimiter between substrings</param>
+		/// <returns>Joined string</returns>
+		std::string Join(const std::string* subStrings, const size_t size, const char delimiter);
+	}
 }
-
-
